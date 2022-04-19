cc5ff173d1b92857592621251d56209685ab88ad
Vismark Juarez <vismarkjuarez@gmail.com>
2022-04-19 00:11:27 -0500

    Cleaning up imports.

--- src/main/java/com/vismark/MicroServiceOne/service/PersonService.java
+++ src/main/java/com/vismark/MicroServiceOne/service/PersonService.java
@@ -3,9 +3,7 @@ package com.vismark.MicroServiceOne.service;
 import com.vismark.MicroServiceOne.domain.Person;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 @Service
