89f83218e28529e328952aaa10160f3db73c79e9
Vismark Juarez <vismarkjuarez@gmail.com>
2022-11-16 19:51:34 -0600

    Added person class back in.

--- src/main/java/com/vismark/MicroServiceOne/domain/Person.java
+++ src/main/java/com/vismark/MicroServiceOne/domain/Person.java
@@ -1,2 +1,62 @@
 package com.vismark.MicroServiceOne.domain;
 
+import com.google.gson.Gson;
+
+import java.util.Objects;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public Person(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Person)) return false;
+        Person person = (Person) o;
+        return getAge() == person.getAge() && Objects.equals(getFirstName(), person.getFirstName()) && Objects.equals(getLastName(), person.getLastName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getFirstName(), getLastName(), getAge());
+    }
+
+    @Override
+    public String toString() {
+        return new Gson().toJson(this);
+    }
+
+
+}
